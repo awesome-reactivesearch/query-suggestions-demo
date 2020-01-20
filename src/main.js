8828a1e467d6c45a3e69863420c1efed69c50d4d
abhinavraj23 <abhinav.23.april@gmail.com>
2020-01-20 23:45:32 +0530

    Changed footer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Search from './Search'
 import {
-	ResultList,
 	ReactiveList,
 	ResultCard,
 } from '@appbaseio/reactivesearch';
@@ -9,6 +8,22 @@ import {
 import './index.css';
 
 class Results extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			showFooter: false
+		}
+	}
+
+	showFooter = ({data}) => {
+		console.log(data)
+		if(!data || !data.length) {
+			return this.setState({ showFooter: false})
+		}
+		this.setState({showFooter: true})
+	}
+
 	render() {
 		return (
 			<div className="row">
@@ -22,63 +37,73 @@ class Results extends React.Component {
 						className="result-list-container"
 						pagination
 						react={{ and: ["Movie"] }}
-						render={({ data }) => (
-							<ReactiveList.ResultCardsWrapper>
-								{data.map(item => (
-									<ResultCard key={item.id}>
-										<ResultCard.Image src={
-											"https://image.tmdb.org/t/p/w500" +
-											item.poster_path
-										} />
-										<ResultCard.Title>
-											<div
-												className="movie-title"
-												dangerouslySetInnerHTML={{
-													__html: item.original_title,
-												}}
-											/>
-										</ResultCard.Title>
+						onData={this.showFooter}
+						render={({ data }) => {
+							return (
+								<>
+									<ReactiveList.ResultCardsWrapper>
+										{data.map(item => (
+											<ResultCard key={item.id}>
+												<ResultCard.Image src={
+													"https://image.tmdb.org/t/p/w500" +
+													item.poster_path
+												} />
+												<ResultCard.Title>
+													<div
+														className="movie-title"
+														dangerouslySetInnerHTML={{
+															__html: item.original_title,
+														}}
+													/>
+												</ResultCard.Title>
 
-										<ResultCard.Description>
-											<div className="flex column justify-space-between">
-												<div>
-													<div>
-														<span className="authors-list">
-															{item.tagline}
-														</span>
-													</div>
-													<div className="ratings-list flex align-center">
-														<span className="stars">
-															{Array(Math.floor((item.vote_average == null ? 0 : item.vote_average) / 2))
-																.fill('x')
-																.map((item, index) => (
-																	<i
-																		className="fas fa-star"
-																		key={index}
-																	/>
-																)) // eslint-disable-line
-															}
-														</span>
-														<span className="avg-rating">
-															({item.vote_count} avg)
+												<ResultCard.Description>
+													<div className="flex column justify-space-between">
+														<div>
+															<div>
+																<span className="authors-list">
+																	{item.tagline}
 																</span>
+															</div>
+															<div className="ratings-list flex align-center">
+																<span className="stars">
+																	{Array(Math.floor((item.vote_average == null ? 0 : item.vote_average) / 2))
+																		.fill('x')
+																		.map((item, index) => (
+																			<i
+																				className="fas fa-star"
+																				key={index}
+																			/>
+																		)) // eslint-disable-line
+																	}
+																</span>
+																<span className="avg-rating">
+																	({item.vote_count} avg)
+																</span>
+															</div>
+														</div>
+														<span className="pub-year">
+															Release: {item.release_year}
+														</span>
 													</div>
-												</div>
-												<span className="pub-year">
-													Release: {item.release_year}
-												</span>
-											</div>
-										</ResultCard.Description>
-									</ResultCard>
-								))}
-							</ReactiveList.ResultCardsWrapper>
-						)}
+												</ResultCard.Description>
+											</ResultCard>
+
+										))}
+									</ReactiveList.ResultCardsWrapper>
+								</>
+							)
+						}}
 					/>
-					<div>
-						Source Code on <a href="https://github.com/appbaseio-apps/query-suggestions-demo">GitHub</a> - Powered by <a href="https://appbase.io/">Appbase</a>
-					</div>
+					{
+						this.state.showFooter ?
+							<div>
+								Source Code on <a class="newanchor" href="https://github.com/appbaseio-apps/query-suggestions-demo">GitHub</a> - Powered by <a class="newanchor" href="https://appbase.io/">Appbase.io</a>
+							</div> : null
+					}
+
 				</div>
-				
+
 			</div>
 
 		);
